8c96483a26aa69a05242a21863f9c66f5d44a0db
Jonathan Bryan <jonnib24@gmail.com>
2020-05-26 05:34:24 -0400

    Working on setting up files

--- Project/app/controllers/articles_controller.rb
+++ Project/app/controllers/articles_controller.rb
@@ -1,4 +1,59 @@
 class ArticlesController < ApplicationController
-    def new
+    def index
+        @articles = Article.all
     end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        #render plain: params[:article].inspect
+        # takes a simple hash with a key of :plain value of params[:article].inspect
+        # params contains the fields coming from the form, this method returns an ActionController::Parameters
+        # Example URL: http://www.example.com/?username=dhh&email=dhh@email.com
+        #   params[:username] = "dhh"
+        #   params[:email] = "dhh@email.com"
+        @article = Article.new(article_params)
+
+        # saves the article to the database 
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+       
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render 'edit'
+        end
+    end
+
+    
+    def destroy
+        # find the article in the db
+        @article = Article.find(params[:id])
+        # delete the article
+        @article.destroy
+   
+        redirect_to articles_path
+    end
+
+    private
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
 end
--- Project/config/routes.rb
+++ Project/config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+  
+  resources :welcome
 
   root 'welcome#index'
+  
 end
