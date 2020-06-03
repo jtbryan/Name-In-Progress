6b26e827cf77cb4cf299667423f9f5192fa241ad
jtbryan <jonnib24@gmail.com>
2020-06-03 16:39:27 -0400

    Currently have users and hashtags working

--- Project/app/controllers/tweets_controller.rb
+++ Project/app/controllers/tweets_controller.rb
@@ -1,5 +1,15 @@
 class TweetsController < ApplicationController
-    def index
-        @value = $client.search("#Floyd")
+    def new
+        @params = request.GET
+        @pref = @params.values[0]
+        @tweet = @params.values[1]
+        if @pref == "hashtag"
+            if @tweet.count('#') == 0
+                @tweet = '#'+@tweet
+            end
+            @value = $client.search(@tweet, result_type: "recent")
+        else
+            @value = $client.search(@tweet, result_type: "recent")
+        end
     end 
 end
